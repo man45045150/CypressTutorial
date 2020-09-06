bdeb9ed4fbd0e57ef553acacf15879ffa7a67276
Aumnuay Niranartkul <man45045150@hotmail.co.th>
2020-09-06 17:06:11 +0700

    completed basic test

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -1,6 +1,15 @@
 describe('Working with inputs',()=>{
+    it('should override the current time',()=>{
+        const date = new Date(2020,3,10).getTime() // return a timestamp
+        cy.clock(date)  
+        cy.log(date)      
+    })
+
     it('should load login page',()=>{
         cy.visit('http://zero.webappsecurity.com/login.html',{timeout:10000})
+        cy.clearCookies({log: true})
+        cy.clearLocalStorage('your item',{log:true})
+        cy.title().should('include','Zero - Log in')
     })
 
     it('should fill username',()=>{
@@ -26,6 +35,8 @@ describe('Working with inputs',()=>{
     })
 
     it('should display error message',()=>{
-        cy.get('.alert-error').should('be.visible')
+        cy.get('.alert-error')
+            .should('be.visible')
+            .and('contain','Login and/or password are wrong.')
     })
 })
